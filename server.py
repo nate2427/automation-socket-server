97986ea9ecc0589999fcf17945ee3ebdec3a2680
Street Coder Nate <natebaker2427@gmail.com>
2021-12-28 17:01:46 -0500

    got the basic rendezvous server working. Sends address and port of the server and client to each other.

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_socketio import SocketIO, emit, send, join_room
+from flask_socketio import SocketIO, disconnect, emit, send, join_room
 from dotenv import load_dotenv # to help load env vars from a file.
 import os
 load_dotenv() # loads the env vars from my .env file
@@ -9,7 +9,11 @@ APPS = [
     'SMBN'
 ]
 # address of the raspberry pi brain backend
-BRAIN_ADDR = None
+brain_info = {
+    "addr": None,
+    "dport": None,
+    "sid": None
+}
 
 # create the flask app and configure it
 app = Flask(__name__)
@@ -20,20 +24,32 @@ socketio_server = SocketIO(app, cors_allowed_origins='*')
 
 # frontend apps will emit this event when they connect, therefore this is the handler
 @socketio_server.on('frontend interface joining server')
-def frontend_joining(data):
+def frontend_joining(dport):
     # get the ip address of the connecting app
     ip_addr = request.environ['REMOTE_ADDR']
-    
-    # establish p2p connection with backend brain
-   
-    # break the frontend's connection to this socket server
-
+    # emit new client connected to raspberry pi with the dport and ip_addr of the client
+    emit('new client connected', {
+        ip_addr: ip_addr,
+        dport: dport
+    })
+    # emit server address to client
+    emit('server address', {
+        ip_addr: brain_info['addr'],
+        dport: brain_info['dport']
+    })
+    print('\n\nSent the Brain: \nip_addr: {} \ndport: {} '.format(ip_addr, dport))
+    print('\n\nSent the Client: \nip_addr: {} \ndport: {} '.format(brain_info['addr'], brain_info['dport']))
+    # disconnect the client from the rendezvous server
+    disconnect()
     
 # the raspberry pi will emit this event when it connects, this is the handler
 @socketio_server.on('backend brain joining server')
-def brain_joining(args):
+def brain_joining(dport_num):
     # get the ip address of the raspberry pi brain and save it
-    BRAIN_ADDR = request.environ['REMOTE_ADDR']
+    brain_info['addr'] = request.environ['REMOTE_ADDR']
+    # also save the destination port the client sends
+    brain_info['dport'] = dport_num
+    print('\nBrain has joined da server!!! \nIP Address: {}\nDestination Port: {}\n'.format(brain_info['addr'], brain_info['dport']))
 
 @app.route("/")
 def hello():
